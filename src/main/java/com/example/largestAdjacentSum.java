39ffe0f3222e63751b6e28151fdfb2f851223d5b
wp <wopoto@gmail.com>
2024-05-31 16:27:35 +0700

    update largestAdjacentSum

--- src/main/java/com/example/largestAdjacentSum.java
+++ src/main/java/com/example/largestAdjacentSum.java
@@ -1,7 +1,27 @@
 package main.java.com.example;
 
 public class largestAdjacentSum {
-    public int largestAdjacentSum(int[] a) {
+
+    public static void main(String[] args) {
+        System.out.println(largestAdjacentSum(new int[]{1, 2, 3, 4}));
+        System.out.println(largestAdjacentSum(new int[]{18, -12, 9, -10}));
+        System.out.println(largestAdjacentSum(new int[]{1,1,1,1,1,1,1,1,1}));
+        System.out.println(largestAdjacentSum(new int[]{1,1,1,1,1,2,1,1,1}));
+    }
+
+    static int largestAdjacentSum(int[] a) {
+        int sum = 0;
+
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] + a[i + 1] > sum) {
+                sum = a[i] + a[i + 1];
+            }
+        }
+        return sum;
+    }
+
+
+    public int largestAdjacentSum2(int[] a) {
         int largestSum = a[0] + a[1];
         if (a.length < 2) {
             return Integer.MIN_VALUE;
