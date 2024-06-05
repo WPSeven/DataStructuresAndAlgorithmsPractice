c1bd371cf289ae8e79f7ab12707a5f851b71d839
wp <wopoto@gmail.com>
2024-06-05 18:37:13 +0700

    decodeArray2

--- src/main/java/com/waiphyothu/decodeArray.java
+++ src/main/java/com/waiphyothu/decodeArray.java
@@ -10,14 +10,13 @@ public class decodeArray {
         System.out.println(decodeArray(new int[]{111, 115, 118, 127, 125})); //4392
         System.out.println(decodeArray(new int[]{1, 1})); // 0
 
-       /* System.out.println("\n\n" + decodeArray2(new int[]{1}));
+        System.out.println("\n\n" + decodeArray2(new int[]{1}));
         System.out.println(decodeArray2(new int[]{0, 1}));
         System.out.println(decodeArray2(new int[]{-1, 0, 1}));
         System.out.println(decodeArray2(new int[]{0, 1, 1, 1, 1, 1, 0, 1}));
         System.out.println(decodeArray2(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}));
         System.out.println(decodeArray2(new int[]{0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1}));
         System.out.println(decodeArray2(new int[]{-1, 0, 0, 1, 1, 0, 1}));
-    */
 
     }
 
@@ -39,7 +38,7 @@ public class decodeArray {
         return sum;
     }
 
-    static int decodeArray2(int[] a) {
+    static int decodeArray12(int[] a) {
         int sum = 0;
 
         for (int i = a.length - 1; i > 0; i--) {
@@ -55,5 +54,33 @@ public class decodeArray {
         return sum;
     }
 
+
+    static int decodeArray2(int[] a) {
+        int sum = 0;
+
+        int position = 0;
+        for (int i = a.length - 1; i > 0; i--) {
+            int digit = 0;
+
+            for (int j = i; j > 0; j--) {
+                if (a[j - 1] != 0) {
+                    i = j;
+                    break;
+                }
+                digit++;
+                i = j;
+            }
+
+            for (int j = 0; j < position; j++) {
+                digit *= 10;
+            }
+            sum += digit;
+
+            position++;
+        }
+
+        if (a[0] < 0) return -sum;
+        return sum;
+    }
 
 }
